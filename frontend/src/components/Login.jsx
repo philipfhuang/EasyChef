65523c123f3b01d6f7a966319240b9ec2d5e8c36
phili <philipp.huang@mail.utoronto.ca>
2023-04-07 00:47:59 -0400

    rewrite login using axios

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,9 @@
+import {useNavigate} from "react-router-dom";
+import axios from "axios";
+
 import logo from '../images/logo-name-h.png'
 import {Button, Form} from "@douyinfe/semi-ui";
 import jwt_decode from "jwt-decode";
-
-import {useNavigate} from "react-router-dom";
 
 export const Login = () => {
 
@@ -21,39 +22,28 @@ export const Login = () => {
             return errors;
         }
 
-        var form = new FormData();
-        form.append("username", values.username);
-        form.append("password", values.password);
-
-        var requestOptions = {
-            method: 'POST',
-            body: form
-        };
-
-        return fetch("http://localhost:8000/accounts/login/", requestOptions)
-                .then(response => {
-                if (response.status === 401) {
+        return axios.post("http://localhost:8000/accounts/login/", values)
+            .then(response => {
+                res = response.data;
+            })
+            .catch(error => {
+                if (error.response.status === 401) {
                     errors.username = "Username or password is incorrect";
                     errors.password = "Username or password is incorrect";
                     return errors;
                 }
-                res = response.json();
-            })
+            });
     }
     const loginSuccess = () => {
-        res.then(result => {
-            localStorage.setItem("token", JSON.stringify(result));
-            const user_id = jwt_decode(result.access).user_id;
-            fetch(`http://localhost:8000/accounts/profile/${user_id}/`, {method: 'GET'})
-                .then(response => response.json())
-                .then(res => {
-                    localStorage.setItem("user", JSON.stringify(res));
-                })
-        })
+        localStorage.setItem("token", JSON.stringify(res));
+        const user_id = jwt_decode(res.access).user_id;
+        axios.get(`http://localhost:8000/accounts/profile/${user_id}/`)
+            .then(response => {
+                localStorage.setItem("user", JSON.stringify(response.data));
+            })
         navigate("/");
     }
     return (
-
         <div style={{width: 300, margin: "0 auto", marginTop: 100}}>
             <img src={logo} alt="logo" style={{width: "100%"}}/>
             <Form style={{marginTop: 10}} validateFields={values => loginValidate(values)} onSubmit={loginSuccess}>
@@ -65,7 +55,6 @@ export const Login = () => {
                 </div>
             </Form>
         </div>
-
     )
 }
 
