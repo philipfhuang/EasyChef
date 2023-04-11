18c17c008c15e951ac7ce261072155d836474e4a
Feiran Huang <philipp.huang@mail.utoronto.ca>
2023-04-11 18:58:17 -0400

    update stater shell

--- startup.sh
+++ startup.sh
@@ -1,8 +1,9 @@
+cd backend/
 python3 -m virtualenv venv -p $(which python3.10)
 source venv/bin/activate
 pip install -r requirements.txt
 python3 easychef/manage.py makemigrations
 python3 easychef/manage.py migrate
 
-cd fronted
+cd ../fronted
 npm install
