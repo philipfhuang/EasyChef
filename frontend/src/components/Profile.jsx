5751e8f14d060de92b0b9b58a545d8d5fcc4216f
phili <philipp.huang@mail.utoronto.ca>
2023-04-13 18:51:06 -0400

    removed unused const to avoid react complaining

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,29 +1,15 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { useParams ,useNavigate } from "react-router-dom";
 import ContentList from './ContentList';
-import {Avatar, Button, Typography, Tabs, TabPane, List, Rating, Spin, BackTop} from '@douyinfe/semi-ui';
-import {IconArrowUp} from "@douyinfe/semi-icons";
+import {Avatar, Button, Typography, Tabs, TabPane} from '@douyinfe/semi-ui';
 
 const Profile = () => {
 
     const { Title, Text } = Typography;
     const [profile, setProfile] = useState({});
-    const [contentToShow, setContentToShow] = useState([]);
     const { user_id } = useParams();
     const navigate = useNavigate();
-
-    const changeContent = (number) => {
-        if (number === 1) {
-            setContentToShow(profile.created_recipes);
-        } else if (number === 2) {
-            setContentToShow(profile.liked_recipes);
-        } else if (number === 3) {
-            setContentToShow(profile.favored_recipes);
-        } else if (number === 4) {
-            setContentToShow(profile.commented_recipes);
-        }
-    }
 
     const editProfileRedirect = () => {
         navigate(`/accounts/profile/edit`);
@@ -34,7 +20,6 @@ const Profile = () => {
             await axios.get(`http://127.0.0.1:8000/accounts/profile/${user_id}/`)
             .then(response => {
                 setProfile(response.data);
-                setContentToShow(response.data.created_recipes);
             })
         }
         fetchData();
