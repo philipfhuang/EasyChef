30e6efefaa48b93a4989eb81a402a897810d83a5
Feiran Huang <philipp.huang@mail.utoronto.ca>
2023-04-13 17:42:10 -0400

    deleted comments

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams } from "react-router-dom";
-import { Form, Button, Avatar, Tooltip, Toast } from '@douyinfe/semi-ui';
+import { Form, Button, Avatar, Toast } from '@douyinfe/semi-ui';
 
 const EditProfile = () => {
     const [first_name, setFirstName] = useState("");
@@ -18,10 +17,8 @@ const EditProfile = () => {
 
     useEffect(() => {
         async function fetchData() {
-            //console.log("1")
             await axios.get(`http://127.0.0.1:8000/accounts/profile/${user_id}/`)
             .then(response => {
-                //console.log(response.data);
                 setFirstName(response.data.first_name);
                 setLastName(response.data.last_name);
                 setEmail(response.data.email);
@@ -32,47 +29,6 @@ const EditProfile = () => {
         fetchData();
     }, []);
 
-    // const handleProfileChange = async () => {
-    //     try {
-    //         let errors = {};
-
-    //         if (first_name === "") {
-    //             errors.first_name = "No First Name";
-    //         }
-    //         if (last_name === "") {
-    //             errors.last_name = "No Last Name";
-    //         }
-    //         if (validateEmail(email) !== "") {
-    //             errors.email = validateEmail(email);
-    //         }
-    //         if (validatePhone(phone_number) !== "") {
-    //             errors.phone_number = validatePhone(phone_number);
-    //         }
-    //         if (Object.keys(errors).length > 0) {
-    //             return errors;
-    //         }
-
-    //         const storedToken = localStorage.getItem('token');
-    //         const accessToken = JSON.parse(storedToken).access;
-
-    //         const config = {
-    //             headers: {Authorization: `Bearer ${accessToken}`}
-    //         };
-
-    //         const formData = {
-    //             first_name: first_name,
-    //             last_name: last_name,
-    //             email: email,
-    //             phone_number: phone_number,
-    //             avatar: avatar
-    //         }
-    //         console.log(formData);
-    //         await axios.patch('http://127.0.0.1:8000/accounts/profile/', formData, config);
-    //     }
-    //     catch (error) {
-    //         console.log("Error uploading profile: ", error);
-    //     }
-    // };
 
     const input_width = 250;
 
@@ -116,37 +72,7 @@ const EditProfile = () => {
         return '';
     }
 
-    // const handleFileChange = async (avatar) => {
-    //     try {
-    //         setAvatar(avatar);
-
-    //         const storedToken = localStorage.getItem('token');
-    //         const accessToken = JSON.parse(storedToken).access;
-
-    //         const config = {
-    //             headers: {Authorization: `Bearer ${accessToken}`}
-    //         };
-            
-    //         const fileData = {
-    //             avatar: avatar
-    //         };
-    //         console.log(fileData);
-    //         await axios.patch('http://127.0.0.1:8000/accounts/profile/', fileData, config);
-    //     }
-    //     catch (error) {
-    //         console.log("Error uploading profile: ", error);
-    //     }
-    // }
-    
-    // console.log(first_name);
-    // console.log(last_name);
-    // console.log(email);
-    // console.log(phone_number);
-
     const submitChange = (value) => {
-
-        console.log(value, "value");
-        console.log("avatar", avatar);
 
         let form = new FormData();
         if (avatarPreview !== "") {
@@ -163,7 +89,6 @@ const EditProfile = () => {
         const config = {
             headers: {Authorization: `Bearer ${accessToken}`}
         };
-        console.log("form",form);
         axios.patch('http://127.0.0.1:8000/accounts/profile/', form, config);
         window.location.reload();
         Toast.success("profile changed successfully!");
@@ -174,16 +99,14 @@ const EditProfile = () => {
     return (
         <div style={{width:'100%', maxWidth:800, margin: "0 auto", marginTop:10, display:"flex", flexDirection:"column", alignItems:"center"}}>
             <Form layout='vertical' onSubmit={value=>{submitChange(value)}}>
-                <Avatar size="extra-large" color='orange' style={{margin: "auto"}} src={avatar}>
+                <Avatar size="extra-large" color='orange' style={{margin: "0 auto"}} src={avatar}>
                     {first_name? first_name.charAt(0).toUpperCase()+last_name.charAt(0).toUpperCase():""}
                 </Avatar>
-                {/* <Form.Upload field='avatar' label='Avatar' onChange={submitAvatar} uploadTrigger="custom" style={{ width: input_width }}>
-                    <Button theme="light">
-                        Upload Picture
-                    </Button>
-                </Form.Upload> */}
-                <br></br>
+                <br/>
+                <br/>
                 <input type="file" accept="image/*" name="avatarPreview" key={avatarPreview ? "avatarPreview" : "no_ap"} onChange={e=>{setAvatarPreview(e.target.files[0])}}/>
+                <br/>
+                <br/>
                 <Form.Input field='first_name' label='FirstName' key={first_name ? "first_name" : "no_fn"} initValue={first_name} validate={validateFirstName} style={{ width: input_width }} type="text"/>
                 <Form.Input field='last_name' label='LastName' key={last_name ? "last_name" : "no_ln"} initValue={last_name} validate={validateLastName} style={{ width: input_width }} type="text"/>
                 <Form.Input field='email' label='email' key={email ? "email" : "no_email"} validate={validateEmail} initValue={email} style={{ width: input_width }}/>
